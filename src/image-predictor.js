9d841bc11539cd6c00f0d2d8c9bd1279d69f5cc0
Fritz Heiden <fritz.heiden@mailbox.org>
2019-03-31 19:20:41 +0200

    added some comments

--- src/image-predictor.js
+++ src/image-predictor.js
@@ -10,29 +10,34 @@ class ImagePredictor {
     this._model = await tf.loadLayersModel("file://./data/model/model.json");
   }
 
+  /**
+   * Predicts a number from image data
+   * @param {Buffer} buffer
+   */
   async predictNumber(buffer) {
     // const image = await Jimp.read("./data/test.jpg");
     const image = await Jimp.read(buffer);
 
-    // image.write(`./data/image-${Date.now()}.jpg`);
+    // image.write(`./data/image-${Date.now()}.jpg`); // save unprocessed image
     const { width, height } = image.bitmap;
-    image.crop(0, 0, width, width);
-    image.greyscale();
-    image.contrast(1);
-    image.invert();
-    image.resize(28, 28);
-    image.write(`./data/image-${Date.now()}.jpg`);
+    image.crop(0, 0, width, width); // make image a square
+    image.greyscale(); // desaturate
+    image.contrast(1); // crank up the contrast to eliminate shadows
+    image.invert(); // invert for the model to understand
+    image.resize(28, 28); // resize so it fits into the model
+    // image.write(`./data/image-${Date.now()}.jpg`); // save processed image
 
+    // make 1 color channel out of 4 rgba
     const { data } = image.bitmap;
     const preparedData = [];
     for (let i = 0; i < data.length; i += 4) {
-      preparedData.push(data[i] / 255);
+      preparedData.push(data[i] / 255); // normalize values for better prediction results
     }
 
-    const tensor = tf.tensor2d([preparedData]);
+    const tensor = tf.tensor2d([preparedData]); // put image on the gpu
     const result = this._model.predict(tensor);
-    const index = await result.argMax(1);
-    const number = (await index.data())[0];
+    const index = await result.argMax(1); // get one hot encoded value
+    const number = (await index.data())[0]; // read data from gpu
     return number;
   }
 }
